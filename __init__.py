1d83e073f009c98eb9dcc6c20d14646a607fd9bd
Yatharth Agarwal <yatharthagrawal@gmail.com>
2024-04-06 13:14:19 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,12 @@
+# python
+# Copy code
+# Example of initialization code in __init__.py
+
+print("Initializing my_module...")
+
+# Define functions or perform other setup tasks
+def setup():
+    print("Setting up my_module...")
+
+# Run setup function when the package is imported
+setup()
